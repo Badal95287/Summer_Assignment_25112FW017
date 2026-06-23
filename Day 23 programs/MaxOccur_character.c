4e9365a37c343ccb2dfdb461a505e761abd58f00
Badal95287 <singhbadal50365@gmail.com>
2026-06-23 21:06:59 +0530

    Added Day 23 folder inside Day 1

---
+++ Day 23 programs/MaxOccur_character.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int freq[256] = {0};
+    int i, max = 0;
+    char maxChar;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        freq[(unsigned char)str[i]]++;
+    }
+
+    
+    for(i = 0; i < 256; i++)
+    {
+        if(freq[i] > max)
+        {
+            max = freq[i];
+            maxChar = i;
+        }
+    }
+
+    printf("Maximum occurring character is '%c' with %d occurrences", maxChar, max);
+
+    return 0;
+}
---
+++ Day 23 programs/NonRepeating_characters.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int freq[256] = {0};
+    int i;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        freq[(unsigned char)str[i]]++;
+    }
+
+    
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(freq[(unsigned char)str[i]] == 1)
+        {
+            printf("First non-repeating character: %c", str[i]);
+            return 0;
+        }
+    }
+
+    printf("No non-repeating character found");
+
+    return 0;
+}
